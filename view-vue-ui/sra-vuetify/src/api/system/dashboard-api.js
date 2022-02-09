c9a137afd86604a77cbffcd9f43bcbc374aa0ea0
CoCoTea <572315466@qq.com>
2022-02-10 00:04:32 +0800

    开发ing...页面渲染系统信息

--- view-vue-ui/sra-vuetify/src/api/system/dashboard-api.js
+++ view-vue-ui/sra-vuetify/src/api/system/dashboard-api.js
@@ -6,3 +6,10 @@ import { request } from '@/utils/axios-util';
 export function getCount() {
     return request('dashboard/getCount', {}, 'GET');
 }
+
+/**
+ * 获取系统信息
+ */
+export function getSystemInfo() {
+    return request('dashboard/getSystemInfo', {}, 'GET');
+}
--- view-vue-ui/sra-vuetify/src/views/system/home/modules/system-info.vue
+++ view-vue-ui/sra-vuetify/src/views/system/home/modules/system-info.vue
@@ -3,13 +3,10 @@
     <v-list dense>
       <v-subheader>系统信息</v-subheader>
       <v-list-item-group color="primary">
-        <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
+        <div class="text-center">
+          <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
+        </div>
+        <v-list-item v-for="(item, i) in items" :key="i">
           <v-list-item-content>
             <v-list-item-title v-text="item.text"></v-list-item-title>
           </v-list-item-content>
@@ -20,17 +17,47 @@
 </template>
 
 <script>
+import {getSystemInfo} from "@/api/system/dashboard-api";
+
 /**
  * 展示系统信息
  */
 export default {
   name: 'SystemInfo',
   data: () => ({
-    items: [
-      {text: 'Real-Time', icon: 'mdi-clock'},
-      {text: 'Audience', icon: 'mdi-account'},
-      {text: 'Conversions', icon: 'mdi-flag'},
-    ],
+    items: [],
+    loading: true
   }),
+  created() {
+    this.getSystemInfo();
+  },
+  methods: {
+    getSystemInfo() {
+      getSystemInfo().then(res => {
+        if (res.code === 200) {
+          this.items.push({text: `操作系统: ${res.data.os}`});
+          this.items.push({text: `服务器名称: ${res.data.serverName}`});
+          this.items.push({text: `服务器IP: ${res.data.serverIp}`});
+          this.items.push({text: `系统架构: ${res.data.serverArchitecture}`});
+          this.items.push({text: `Java名称: ${res.data.javaName}`});
+          this.items.push({text: `Java版本: ${res.data.javaVersion}`});
+          this.items.push({text: `java安装路径: ${res.data.javaPath}`});
+          this.items.push({text: `项目路径: ${res.data.projectPath}`});
+          this.items.push({text: `服务运行时长: ${res.data.runningTime} 秒`});
+          this.items.push({text: `CPU系统使用率: ${res.data.cpuSystemUsed}`});
+          this.items.push({text: `CPU用户使用率: ${res.data.cpuUserUsed}`});
+          this.items.push({text: `CPU空闲率: ${res.data.cpuFree}`});
+          this.items.push({text: `CPU核心数: ${res.data.cpuCount} 个`});
+          this.items.push({text: `内存总大小: ${(res.data.memoryTotalSize / 1024 / 1024 / 1024).toFixed(2)}G`});
+          this.items.push({text: `已用内存: ${(res.data.memoryUsedSize / 1024 / 1024 / 1024).toFixed(2)}G`});
+          this.items.push({text: `磁盘总大小: ${(res.data.diskTotalSize / 1024 / 1024 / 1024).toFixed(2)}G`});
+          this.items.push({text: `磁盘剩余大小: ${(res.data.diskFreeSize / 1024 / 1024 / 1024).toFixed(2)}G`});
+          this.items.push({text: `磁盘路径: ${res.data.diskPath}`});
+          this.items.push({text: `磁盘分隔符: ${res.data.diskSeparator}`});
+          this.loading = false;
+        }
+      });
+    },
+  }
 }
 </script>
