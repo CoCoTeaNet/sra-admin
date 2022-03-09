c3c9645143f5db47c63dd8b563724d322f90e952
CoCoTea <572315466@qq.com>
2022-03-09 22:05:47 +0800

    :art: 重构前端-面包屑开发ing

--- sra-ui/src/layout/modules/AdminBreadcrumb.vue
+++ sra-ui/src/layout/modules/AdminBreadcrumb.vue
@@ -1,17 +1,32 @@
 <template>
   <el-breadcrumb separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item
-    ><a href="/">promotion management</a></el-breadcrumb-item
-    >
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="item in itemList" :to="{ path: item.href }">
+      {{ item.text }}
+    </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {RouteLocationMatched, useRoute} from "vue-router";
+
+const itemList = ref<RouteLocationMatched[]>([]);
+
+onMounted(() => {
+  getRouterMatch();
+})
+
+/**
+ * 获取路由导航地址
+ */
+const getRouterMatch = () => {
+  let list = useRoute().matched;
+  let items: any[] = []
+  list.forEach((item, index) => {
+    items.push({text: item.meta.title, disabled: index === list.length - 1, href: item.path});
+  });
+  itemList.value = items;
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
