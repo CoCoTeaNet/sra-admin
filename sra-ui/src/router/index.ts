4f7481d4177e947e3e4d509e42b0e9103e845a9f
liangjw <572315466@qq.com>
2022-04-07 11:19:21 +0800

    :lipstick: history路由修改为hash路由

--- sra-ui/src/router/index.ts
+++ sra-ui/src/router/index.ts
@@ -1,8 +1,8 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 import {routes} from "@/router/modules/routes";
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: routes
 })
 
