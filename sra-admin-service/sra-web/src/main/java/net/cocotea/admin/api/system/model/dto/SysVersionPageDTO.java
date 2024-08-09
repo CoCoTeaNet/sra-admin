e7f880fe3eb26d692c708a416e38ed7e035f88db
CoCoTea <572315466@qq.com>
2024-08-09 22:35:28 +0800

    优化：系统用户管理列表接口-屏蔽多余参数

--- sra-admin-service/sra-web/src/main/java/net/cocotea/admin/api/system/model/dto/SysVersionPageDTO.java
+++ sra-admin-service/sra-web/src/main/java/net/cocotea/admin/api/system/model/dto/SysVersionPageDTO.java
@@ -3,8 +3,7 @@ package net.cocotea.admin.api.system.model.dto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-import net.cocotea.admin.api.system.model.vo.SysVersionVO;
-import org.sagacity.sqltoy.model.Page;
+import net.cocotea.admin.common.model.ApiPageDTO;
 
 import jakarta.validation.constraints.NotNull;
 import java.io.Serial;
@@ -13,11 +12,26 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Accessors(chain = true)
-public class SysVersionPageDTO extends Page<SysVersionVO> implements Serializable {
+public class SysVersionPageDTO extends ApiPageDTO implements Serializable {
 	@Serial
 	private static final long serialVersionUID = 2569886533383934522L;
 
 	@NotNull(message = "查询参数为空")
-	private SysVersionVO sysVersion;
+	private Query sysVersion;
+
+	@Data
+	public static class Query {
+
+		/**
+		 * 系统平台名称
+		 */
+		private String platformName;
+
+		/**
+		 * 更新版本号
+		 */
+		private String updateNo;
+
+	}
 
 }
